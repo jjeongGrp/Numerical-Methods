c3ee5340df7eed77e5a4074732cf7f205bc9baa5
Jeong <bebopone@naver.com>
2022-09-22 23:04:56 -0500

    Chapter1.Sec1

--- Python3Intro.py
+++ Python3Intro.py
@@ -36,5 +36,29 @@ name = rec[0] + ' ' + rec[1]
 print(name)
 print(rec[0:2])
 
+# Lists
+
+a = [1.0, 2.0, 3.0]  # Creat a list
+a.append(4.0)      # Append 4.0 to list
+print(a)
+
+a.insert(0, -1.0)   # Insert 0.0 in position 0
+print(a)
+print(len(a))
+a[2:4] = [-2.0, -3.0, -5.0] # Modify selected elements
+print(a)
+
+a1 = [1., 2., 3.]
+b1 = a1             # b1 is an alias of a1
+b1[0]=5.            # change b1
+print(a1)           # The change is reflected in a1
+print(a1[0])
+
+c1 = a1[:]          # c1 is an independent copy of a1
+c1[0] = -1.         # change c1
+print(c1)           
+print(a1)           # a1 is not affected by the change
+
+
 
 
